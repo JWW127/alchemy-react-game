4f2aca3e3bc8250107263cf4af9360dfa6a32227
Joey <joseph.w.woolever@gmail.com>
2021-11-25 02:01:00 -0600

    css reset, bg assets, bg fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Friend</h1>
+      <div className="background">
+        
+      </div>
     </div>
   );
 }
