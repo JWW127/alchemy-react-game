904b0145a43815b5f6507d09645105bb47f0a061
Joey <joseph.w.woolever@gmail.com>
2021-11-27 22:25:06 -0600

    started ingredient component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Ingredient } from './components/Ingredient';
 
 function App() {
   return (
     <div className="App">
       <div className="background">
-        
+        <main>
+          <div className="ingredientContainer">
+          
+          </div>
+          <div></div>
+       
+        </main>
       </div>
     </div>
   );
---
+++ src/components/Ingredient.tsx
@@ -0,0 +1,9 @@
+
+
+export const Ingredient: React.FC = () => {
+  return (
+      <div className="ingredient">
+      <h1>🔥</h1>
+      </div>
+  )
+}
