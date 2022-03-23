09a0ac383da9256a7c2e2723eb8a79cb43f7e25a
Joey <joseph.w.woolever@gmail.com>
2022-03-23 01:59:35 -0500

    styles added
    Signed-off-by: Joey <joseph.w.woolever@gmail.com>

--- src/allIngredients.tsx
+++ src/allIngredients.tsx
@@ -12,8 +12,7 @@ import {
 import { HiFire } from "react-icons/hi";
 import { WiWindy } from "react-icons/wi";
 import { BsWind } from "react-icons/bs";
-//import {  SiTailwindcss} from 'react-icons/si'
-import { FaMountain } from "react-icons/fa"
+import { FaMountain } from "react-icons/fa";
 import { IconType } from "react-icons";
 
 export interface ingredient {
@@ -21,95 +20,95 @@ export interface ingredient {
   isActive: boolean;
   icon?: IconType;
   color: string;
-  mix?: string
+  mix?: string;
 }
 
 export const ingredientsArr: ingredient[] = [
-    {
+  {
     name: "fire",
     icon: HiFire,
     color: "red",
     isActive: true,
-    },
-    {
+  },
+  {
     name: "water",
     icon: GiWaterDrop,
     color: "blue",
     isActive: true,
-    },
-    {
+  },
+  {
     name: "air",
     icon: WiWindy,
     color: "white",
     isActive: true,
-    },
-    {
+  },
+  {
     name: "earth",
     icon: GiGroundSprout,
     color: "brown",
     isActive: true,
-    },
-    {
+  },
+  {
     name: "steam",
     icon: GiSteam,
     color: "white",
     mix: "firewater",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "ice",
     icon: GiIceCube,
     color: "blue",
     mix: "airwater",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "mountain",
     icon: FaMountain,
     color: "blue-gray",
     mix: "earthearth",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "rain",
     icon: GiHeavyRain,
     color: "raining",
     mix: "waterwater",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "snow",
     icon: GiSnowing,
     color: "white",
     mix: "icerain",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "wind",
     icon: BsWind,
     color: "windy",
     mix: "airair",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "tornado",
     icon: GiTornado,
     color: "white",
     mix: "windwind",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "campfire",
     icon: GiCampfire,
     color: "burning",
     mix: "firefire",
-    isActive: false
-    },
-    {
+    isActive: false,
+  },
+  {
     name: "sun",
     icon: GiStripedSun,
     color: "sunny",
     mix: "firecampfire",
-    isActive: false
-    }
+    isActive: false,
+  },
 ];
